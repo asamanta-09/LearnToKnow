22cd2bf845827e4e013072c8ee7a05e700181919
Ankan Samanta <ankan.samanta8017@gmail.com>
2025-06-01 22:42:32 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(
   cors({
-    origin: "https://learntoknow.vercel.app",
+    origin: ["http://localhost:5173", "https://learntoknow.vercel.app"],
     credentials: true,
   })
 );
